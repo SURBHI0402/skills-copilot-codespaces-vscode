b9fbc801682ced825ace4ca7957504b7c707227a
Surbhi D Raut <81684867+SURBHI0402@users.noreply.github.com>
2023-07-22 13:40:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillMember{
+    return{
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller:'SkillsMemberController',
+        controllerAs:'vm',
+        bindToController:true,
+        scope:{
+            member:'=',
+        }
+    };
+
+}
